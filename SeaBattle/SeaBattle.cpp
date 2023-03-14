e8a5326858dc86d8afb9f09e4a23cdd739e30ff0
kosmonafte <itrzn@mail.ru>
2023-03-15 02:18:18 +0300

    battleColor

--- SeaBattle/SeaBattle.cpp
+++ SeaBattle/SeaBattle.cpp
@@ -3,40 +3,49 @@
 
 #include <iostream>
 #include <windows.h>
+#include <stdio.h>
 using namespace std;
 char** createField(int size);
 void printBattleField(char** enemy, char** my, int size);
+void printMyBattleField(char** enemy, char** my, int size);
 void createShip(char** array, char* str);
 int checkShip(char* decks, int size);
 int checkField(char** field, char* decks);
-int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr);
-int mainMenu(bool* shipsFullPtr);
+int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr, bool* enemyShipsFullPtr, HANDLE console);
+int mainMenu(bool* shipsFullPtr, bool* pausePtr, HANDLE console);
 int automaticFillField(char** efield, char** mfield, int size, bool* shipsFullPtr);
 int automaticFF(char** field, bool* shipsFullPtr, int size);
+int battle(char** enemyField, char** myField, char** tempField, int size, HANDLE console);
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
     srand(time(NULL));
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
     int size = 11;
     char** enemyField = createField(size);
     char** myField = createField(size);
+    char** tempField = createField(size);
     bool menu = true; // Окно стартового меню
     bool arrangeShips = false; // Окно расстановки кораблей
     bool autoArrangeShips = false; // Окно автоматической расстановки кораблей
-    bool battle = false; // Окно сражения
-    bool shipsFull = false; bool* shipsFullPtr = &shipsFull; // Расставлены корабли или нет
-    bool enemyShipsFull = false; bool* enemySshipsFullPtr = &shipsFull;
+    bool battleField = false; // Окно сражения
+    bool shipsFullFlag = false; bool* shipsFullFlagPtr = &shipsFullFlag; // Расставлены корабли или нет
+    bool pauseFlag = false; bool* pauseFlagPtr = &pauseFlag;
+    bool enemyShipsFull = false; bool* enemyShipsFullPtr = &enemyShipsFull;
     while (true) {
         if (menu) { //  Окно меню
-            int checkMenu = 0;
-            checkMenu = mainMenu(shipsFullPtr);
+            int checkMenu = mainMenu(shipsFullFlagPtr, pauseFlagPtr, console);
             if (checkMenu == 1) {
-                if (shipsFull) {
+                if (shipsFullFlag) {
+                    if (pauseFlag) {
+                        delete[]tempField;
+                        tempField = createField(size);
+                    }
                     menu = false;
                     arrangeShips = false;
                     autoArrangeShips = false;
-                    battle = true;
+                    battleField = true;
                     continue;
                 }
                 else {
@@ -44,34 +53,53 @@ int main()
                 } 
             }
             else if (checkMenu == 2) {
-                automaticFF(myField, shipsFullPtr, size);
+                if (pauseFlag) {
+                    continue;
+                }
+                automaticFF(enemyField, enemyShipsFullPtr, size);
+                automaticFF(myField, shipsFullFlagPtr, size);
                 continue;
             }
             else if (checkMenu == 3) {
+                if (pauseFlag) {
+                    continue;
+                }
                 menu = false;
                 arrangeShips = true;
                 autoArrangeShips = false;
-                battle = false;
+                battleField = false;
                 continue;
+            }
+            else if (checkMenu == 4) {
+                if (pauseFlag) {
+                    menu = false;
+                    arrangeShips = false;
+                    autoArrangeShips = false;
+                    battleField = true;
+                    continue;
+                }
+                else {
+                    continue;
+                }
             }
             else {
                 continue;
             }
         }
         if (arrangeShips) { // Окно расстановки кораблей
-            int check = fillField(enemyField, myField, size, shipsFullPtr);
-            if (check == 1) {
+            int checkArrangeShips = fillField(enemyField, myField, size, shipsFullFlagPtr, enemyShipsFullPtr, console);
+            if (checkArrangeShips == 1) {
                 arrangeShips = false;
-                battle = true;
+                battleField = true;
             }
-            else if (check == 2) {
+            else if (checkArrangeShips == 2) {
                 delete[]myField;
                 myField = createField(size);
-                shipsFull = false;
+                shipsFullFlag = false;
                 continue;
             }
-            else if (check == 3) {
-                if (!shipsFull) {
+            else if (checkArrangeShips == 3) {
+                if (!shipsFullFlag) {
                     delete[]myField;
                     myField = createField(size);
                 }
@@ -79,50 +107,62 @@ int main()
                 arrangeShips = false;
                 continue;
             }
+            else if (checkArrangeShips == 4) {
+                automaticFF(myField, shipsFullFlagPtr, size);
+                continue;
+            }
         }
-        //if (autoArrangeShips) { // Окно автоматической расстановки кораблей
-        //    int check = automaticFillField(enemyField, myField, size, shipsFullPtr);
-        //    if (check == 1) {
-        //        autoArrangeShips = false;
-        //        battle = true;
-        //    }
-        //    else if (check == 2) {
-        //        delete[]myField;
-        //        myField = createField(size);
-        //        shipsFull = false;
-        //        continue;
-        //    }
-        //    else if (check == 3) {
-        //        if (!shipsFull) {
-        //            delete[]myField;
-        //            myField = createField(size);
-        //        }
-        //        menu = true;
-        //        arrangeShips = false;
-        //        continue;
-        //    }
-        //}
-        if (battle) { // Окно сражения  
-            automaticFF(enemyField, enemySshipsFullPtr, size);
-            char* str = new char[3];
-            char** tempField = createField(size);
-            while (true) {
-                system("cls");
-                printBattleField(tempField, myField, size);
-                cout << "Выстрел: ";
-                cin >> str;
-                if (enemyField[str[1] - 47][str[0] - 64] == '#') {
-                    tempField[str[1] - 47][str[0] - 64] = 'X';
-                }
-                else {
-                    tempField[str[1] - 47][str[0] - 64] = ' ';
-                }
+        if (battleField) { // Окно сражения  
+            
+            int checkBattle = battle(enemyField, myField, tempField, size, console);
+            if (checkBattle == 1) {
+                battleField = false;
+                menu = true;
+                pauseFlag = true;
+                continue;
+            }
+            else if (checkBattle == 2) {
+                battleField = false;
+                menu = true;
+                pauseFlag = false;
+                arrangeShips = false;
+                shipsFullFlag = false;
+                delete[]tempField;
+                delete[]enemyField;
+                tempField = createField(size);
+                enemyField = createField(size);
+                continue;
             }
         }
     }
 }
 
-int automaticFF(char** field, bool* shipsFullPtr, int size) {
+int battle(char** enemyField, char** myField, char** tempField, int size, HANDLE console) {
+    char* str = new char[3];
+    while (true) {
+        system("cls");
+        SetConsoleTextAttribute(console, FOREGROUND_BLUE);
+        printBattleField(tempField, myField, size);
+        SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        cout << endl;
+        cout << "Выстрел: ";
+        cin >> str;
+        if (!strcmp(str, "pause")) {
+            return 1;
+        }
+        else if (!strcmp(str, "exit")) {
+            return 2;
+        }
+        if (enemyField[str[1] - 47][str[0] - 64] == '#') {
+            tempField[str[1] - 47][str[0] - 64] = 'X';
+        }
+        else {
+            tempField[str[1] - 47][str[0] - 64] = ' ';
+        }
+    }
+}
+
+int automaticFF(char** field, bool* shipsFullPtr, int size) {  // Автоматическая расстановка кораблей
     for (int i = 1; i < size; i++) {
         for (int j = 1; j < size; j++) {
             field[i][j] = '.';
@@ -238,7 +278,7 @@ int automaticFF(char** field, bool* shipsFullPtr, int size) {
     return 0;
 }
 
-int automaticFillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {
+int automaticFillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  //  Первый вариант автоматической расстановки кораблей, теперь не использую, использую automaticFF
     int i = 0; int timer = 25;
     char* decks = new char[6];
     bool fullShips = false;
@@ -446,10 +486,11 @@ int automaticFillField(char** efield, char** mfield, int size, bool* shipsFullPt
     }
 }
 
-int mainMenu(bool* shipsFullPtr) {
+int mainMenu(bool* shipsFullPtr, bool* pausePtr, HANDLE console) {
     system("cls");
     int start = 0;
-    cout << "Морской бой!" << endl;
+    cout << endl;
+    SetConsoleTextAttribute(console, FOREGROUND_BLUE);
     cout << "===============================================================================" << endl;
     cout << " ####   #####   ####      #####    ####   ######  ######  ##      #####     ###" << endl;
     cout << "##      ##     ##  ##     ##  ##  ##  ##    ##      ##    ##      ##        ###" << endl;
@@ -457,12 +498,40 @@ int mainMenu(bool* shipsFullPtr) {
     cout << "    ##  ##     ##  ##     ##  ##  ##  ##    ##      ##    ##      ##           " << endl;
     cout << " ####   #####  ##  ##     #####   ##  ##    ##      ##    ######  #####     ###" << endl;
     cout << "===============================================================================" << endl;
+    
     cout << endl;
     cout << endl;
-    cout << "1 - Начать сражение";
-    (*shipsFullPtr) ? cout << " (Корабли расставлены!)" << endl : cout << " (Необходимо расставить корабли!)" << endl;
-    cout << "2 - Автоматическая расстановка кораблей" << endl;
-    cout << "3 - Расставить корабли в ручную" << endl;
+    if (*shipsFullPtr) {
+        SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        cout << "1 - Начать сражение";
+        (*shipsFullPtr) ? cout << " (Корабли расставлены!)" << endl : cout << " (Необходимо расставить корабли!)" << endl;
+    }
+    else {
+        SetConsoleTextAttribute(console, FOREGROUND_INTENSITY);
+        cout << "1 - Начать сражение";
+        (*shipsFullPtr) ? cout << " (Корабли расставлены!)" << endl : cout << " (Необходимо расставить корабли!)" << endl;
+    }
+    if (*pausePtr) {
+        SetConsoleTextAttribute(console, FOREGROUND_INTENSITY);
+        cout << "2 - Автоматическая расстановка кораблей" << endl;
+        cout << "3 - Расставить корабли в ручную" << endl;
+    }
+    else {
+        SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        cout << "2 - Автоматическая расстановка кораблей" << endl;
+        cout << "3 - Расставить корабли в ручную" << endl;
+    }
+    if (*pausePtr) {
+        SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        cout << "4 - Продолжить сражение";
+        (*pausePtr) ? cout << " (Игра начата)" << endl : cout << " (Игра еще не начата!)" << endl;
+    }
+    else {
+        SetConsoleTextAttribute(console, FOREGROUND_INTENSITY);
+        cout << "4 - Продолжить сражение";
+        (*pausePtr) ? cout << " (Игра начата)" << endl : cout << " (Игра еще не начата!)" << endl;
+    }
+    
     cout << endl;
     cin >> start;
     if (start == 1) {
@@ -474,23 +543,30 @@ int mainMenu(bool* shipsFullPtr) {
     else if (start == 3) {
         return 3;
     }
+    else if (start == 4) {
+        return 4;
+    }
     else {
         return 0;
     }
 }
 
-int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  // Функиця расстановки кораблей
+int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr, bool* enemyShipsFullPtr, HANDLE console) {  // Функиця расстановки кораблей в ручную
     int i = 0;
     char* decks = new char[6];
     bool fullShips = false;
+    automaticFF(efield, enemyShipsFullPtr, size);
     if (!*shipsFullPtr) {
         while (!fullShips) {
             system("cls");
+            SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
             cout << " Морской бой!" << endl;
             cout << " -------------------------------------------" << endl;
-            printBattleField(efield, mfield, size);
+            SetConsoleTextAttribute(console, FOREGROUND_BLUE);
+            printMyBattleField(efield, mfield, size);
             cout << endl;
-            cout << "Противник уже расставил корабли, дело за тобой!" << endl;
+            //cout << "Противник уже расставил корабли, дело за тобой!" << endl;
+            SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
             cout << "Используй координатную сетку для указания места установки корабля." << endl;
             cout << "Чтобы поставить четырех палубный корабль укажи первую и последнюю клетку через знак '-'." << endl;
             cout << "Например A1-A4, корабли можно ставить только горизонтально или вертикально!" << endl;
@@ -577,6 +653,7 @@ int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  // 
         cout << "1 - Начать сражение" << endl;
         cout << "2 - Переставить корабли" << endl;
         cout << "3 - Выйти в меню" << endl;
+        cout << "4 - Автоматическая расстановка" << endl;
         cin >> check;
         if (check == 1) {
             return 1;
@@ -586,6 +663,9 @@ int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  // 
         }
         else if (check == 3) {
             return 3;
+        }
+        else if (check == 4) {
+            return 4;
         }
         else {
             return 0;
@@ -593,9 +673,11 @@ int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  // 
     }
     else {
         system("cls");
+        SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
         cout << " Морской бой!" << endl;
         cout << " -------------------------------------------" << endl;
-        printBattleField(efield, mfield, size);
+        SetConsoleTextAttribute(console, FOREGROUND_BLUE);
+        printMyBattleField(efield, mfield, size);
         cout << endl;
         /*cout << "Противник уже расставил корабли, дело за тобой!" << endl;
         cout << "Используй координатную сетку для указания места установки корабля." << endl;
@@ -603,10 +685,12 @@ int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  // 
         cout << "Например A1-A4, корабли можно ставить только горизонтально или вертикально!" << endl;
         cout << "Для сброса введи: reset. Для выхода в меню: exit." << endl;*/
         int check = 0;
+        SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
         cout << endl << "Корабли расставлены!" << endl;
         cout << "1 - Начать сражение" << endl;
         cout << "2 - Переставить корабли" << endl;
         cout << "3 - Выйти в меню" << endl;
+        cout << "4 - Автоматическая расстановка" << endl;
         cin >> check;
         if (check == 1) {
             return 1;
@@ -616,6 +700,9 @@ int fillField(char** efield, char** mfield, int size, bool* shipsFullPtr) {  // 
         }
         else if (check == 3) {
             return 3;
+        }
+        else if (check == 4) {
+            return 4;
         }
         else {
             return 0;
@@ -744,6 +831,16 @@ void printBattleField(char** enemy, char** my, int size) {          //  Функ
             cout << " " << enemy[i][j];
         }
         cout << "\t";
+        for (int j = 0; j < size; j++) {
+            cout << " " << my[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void printMyBattleField(char** enemy, char** my, int size) {          //  Функиця выводит на экран два игровых поля в горизонтальной виде
+    cout << " Ваше поле" << endl;
+    for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             cout << " " << my[i][j];
         }
